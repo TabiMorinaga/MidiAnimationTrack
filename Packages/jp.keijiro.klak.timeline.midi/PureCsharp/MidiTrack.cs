1f1c668b1676148ed5fa7cc28cce40d7ae873447
Tabi Morinaga <tabikame29@outlook.jp>
2020-11-11 18:11:08 +0900

    MidiTrackとMidiTrackPlayerを追加

---
+++ Packages/jp.keijiro.klak.timeline.midi/PureCsharp/MidiTrack.cs
@@ -0,0 +1,19 @@
+﻿namespace Klak.Timeline.Midi
+{
+    public class MidiTrack
+    {
+        #region Serialized variables
+
+        public string name = "No Name";
+        public float tempo = 120;
+        public uint duration;
+        public uint ticksPerQuarterNote = 96;
+        public NoteEvent[] events;
+        public float DurationInSecond
+        {
+            get { return duration / tempo * 60 / ticksPerQuarterNote; }
+        }
+
+        #endregion
+    }
+}
